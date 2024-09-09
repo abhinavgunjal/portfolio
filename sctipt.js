2fc29c0b341f78aca7818acc7f741468ff647d46
abhinavgunjal <83082319+abhinavgunjal@users.noreply.github.com>
2024-09-09 18:10:57 +0530

    Delete sctipt.js

--- sctipt.js
+++
@@ -1,7 +0,0 @@
-var typed = new Typed((".skills"),{
-    strings : ["Frontend Developer","Youtuber","Ui Designer"],
-    typeSpeed : 100,
-    backSpeed : 100,
-    backDelay : 1000,
-    loop : true
-});
